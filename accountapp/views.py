fc8f9c7efae7840f740443060d9cfa021da9edeb
jeong <hi_coffeeci@likelion.org>
2021-07-08 12:56:23 +0900

    7/8 gis_6ban class commit 4

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def hello_world(request):
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text':'POST METHOD!'})
-    else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
