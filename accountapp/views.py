3e8582b7cc8eeac1cdbf45cd5e7baa1172ac9075
jeong <hi_coffeeci@likelion.org>
2021-07-12 11:31:29 +0900

    7/12 gis_6ban class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from accountapp.models import HelloWorld
+
 
 def hello_world(request):
     if request.method == "POST":
 
         temp = request.POST.get('input_text')
-        return render(request, 'accountapp/hello_world.html', context={'text': temp})
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'accountapp/hello_world.html', context={'new_hello_world': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
