31485e3490216a04caf1ec837ae8e9b4847294b1
jeong <hi_coffeeci@likelion.org>
2021-07-26 12:25:35 +0900

    7/26 gis_6ban class commit 3

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -8,3 +8,4 @@ class ProfileCreationForm(ModelForm):
     class Meta:
         model = Profile
         fields = ['image', 'nickname', 'message']
+        # 클라이언트가 user에 접근하지 못하도록 user을 확인한 후에 서버에서 요청에 응답을 할 예정
