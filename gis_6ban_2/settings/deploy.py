dec641422fdd3c358cb2d641a943f86e268674e1
jeong <hi_coffeeci@likelion.org>
2021-09-27 11:51:13 +0900

    9/27 gis_6ban_2 class commit 2

--- gis_6ban_2/settings/deploy.py
+++ gis_6ban_2/settings/deploy.py
@@ -1,25 +1,16 @@
 from .base import *
 
-env_list = dict()
+def read_secret(secret_name):
 
-local_env = open(os.path.join(BASE_DIR, '.env'), encoding='utf-8')  # 불러들이기
-# 운영체제의 경로탐색에서 BASE_DIR과 env 파일을 join 즉, 최상위 폴더 gis_6ban_1에 있는 env 파일을 oepn
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.lstrip().rstrip()
+    file.close()
+    return secret
 
-while True:
-    line = local_env.readline()  # 라인 한줄씩 일기
-    if not line:  # 라인이 없다면 break
-        break
-    line = line.replace('\n', '')
-    start = line.find('=')  # =의 위치를 판단하고 인덱스 알려준 후 start에 저장
-    key = line[:start]  # 시크릿 키에서 =을 기준으로 왼쪽을 key 오른쪽을 value로 판단
-    value = line[start + 1:]
-    env_list[key] = value
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -34,8 +25,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'password1234',
+        'USER': read_secret('MARIADB_USER'),
+        'PASSWORD': read_secret('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
